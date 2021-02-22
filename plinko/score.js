87e86b368de582e7963c1e97ac8d5db170614497
Davisom Correa <correa.davisom@gmail.com>
2021-02-22 09:50:47 -0400

    First Commit, Base Project github.com/StephenGrider/MLKits.git

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,58 @@
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
+/* Ex Layout: [dropPosition, bounciness, ball size, bucket]
+const outputs = [
+	[10, 0.5, 16, 1],
+  [200, 0.5, 16, 4],
+  [350, 0.5, 16, 4],
+  [600, 0.5, 16, 5]
+];
+*/
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
-function runAnalysis() {
-  // Write code here to analyze stuff
+function runAnalysis() { 
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, 10);
+
+  let numberCorrect = 0;
+  for (let i=0; i < testSet.length; i++){
+    const bucket = knn(trainingSet, testSet[i][0]);
+    if (bucket === testSet[i][3]) {
+      numberCorrect++;
+    }
+  }
+
+  console.log('Accuracy: ', numberCorrect / testSetSize)
 }
 
+function knn(data, point) {
+  return _.chain(data)
+  .map(row => [distance(row[0], point), row[3]])
+  .sortBy(row => row[0])
+  .slice(0, k)
+  .countBy(row => row[1])
+  .toPairs()
+  .sortBy(row => row[1])
+  .last()
+  .first()
+  .parseInt()
+  .value();
+}
+
+function distance(pointA, pointB) {
+	return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+}
